7951c26e43edd6b304c07ad34abcb7022e4e0028
Fernando Schettini <fernandoschettini@Outlook.com>
2023-09-28 16:37:31 -0300

    [:memo:DOCS]: gb colab

--- project_setup/install_tools.sh
+++ project_setup/install_tools.sh
@@ -23,7 +23,7 @@ pip install jupyter
 
 # Install Julia
 JULIA_VER=`cut -d '.' -f -2 <<< "$JULIA_VERSION"`
-echo "Installing Julia $JULIA_VERSION on the current Colab Runtime..."
+echo "Installing Julia $JULIA_VERSION..."
 BASE_URL="https://julialang-s3.julialang.org/bin/linux/x64"
 URL="$BASE_URL/$JULIA_VER/julia-$JULIA_VERSION-linux-x86_64.tar.gz"
 wget -nv $URL -O /tmp/julia.tar.gz # -nv means "not verbose"
